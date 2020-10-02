0f9bd2a0c5adefacb61ab0497c2188b888e0ec05
piyal-das <66130612+piyal-das@users.noreply.github.com>
2020-10-02 22:00:59 +0600

    port and nodemon fixed

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const port = 5000;
 
+app.get('/', (req, res) =>{
+  res.send('its working')
+})
+
 
 app.use(cors());
 app.use(bodyParser.json());
